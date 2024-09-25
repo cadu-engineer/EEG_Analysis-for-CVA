66416f5e27c8fe89faab8c3483a3fb3d6677eeba
cadu-engineer <carloseduardo2609@outlook.com>
2024-09-25 17:41:22 -0300

    download_data

---
+++ download_data2.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup as bs
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+from time import sleep
+import pyautogui
+options = Options()
+navegador = webdriver.Chrome(options=options)
+
+navegador.get("https://user:password@isip.piconepress.com/projects/nedc/data/tuh_eeg/tuh_eeg_abnormal/v3.0.1/edf/train/normal/01_tcp_ar/")
+
+links = navegador.find_elements(By.TAG_NAME, "a")
+
+for link in links[5:-1]:
+    actions = ActionChains(navegador)
+    actions.context_click(link).perform()
+    pyautogui.press("down")
+    # sleep(0.3)
+    pyautogui.press("down")
+    # sleep(0.3)
+    pyautogui.press("down")
+    # sleep(0.3)
+    pyautogui.press("down")
+    # sleep(0.3)
+    pyautogui.press("enter")
+    sleep(1.5)
+    pyautogui.press("enter")
+    sleep(0.7)
+
+
+
+
+
+
+
